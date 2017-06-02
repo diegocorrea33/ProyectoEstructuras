c2cbb257241bbd01f6e9ab89d4e9d6cb1afcd9c1
diegocorrea33 <diego.correa33@hotmail.es>
2017-06-02 15:08:39 -0500

    D - Carga 4

--- Proyectofinal/src/proyectofinal/Proyectofinal.java
+++ Proyectofinal/src/proyectofinal/Proyectofinal.java
@@ -1,12 +1,33 @@
 
 package proyectofinal;
 
+import java.util.Scanner;
+
 
 public class Proyectofinal {
 
+    static Scanner sc = new Scanner(System.in);
     
     public static void main(String[] args) {
        
+        int op;
+        
+        do{
+            op = Menu();
+            switch (op) {
+                case 1:
+                    break;
+            }
+        }while(op != 0);
+        
+    }
+    
+    public static int Menu(){
+    
+        int op;
+        System.out.println("");
+        op = sc.nextInt();
+        return op;
     }
     
 }
